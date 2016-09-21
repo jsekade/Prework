dbe3c48194f43d513779b3746c9fac383b21623f
jsekade <jsekade@gmail.com>
2016-09-21 18:26:14 +0200

    Add Fizzbuzz.rb

---
+++ Fizzbuzz.rb
@@ -0,0 +1,13 @@
+for number in (0..100)
+
+	if number % 3 == 0 && number % 5 == 0
+		puts "FizzBuzz"	
+	elsif number % 3 == 0
+		puts"Fizz"
+	elsif number % 5 == 0
+		puts "Buzz"	
+	else
+		puts number
+
+end
+end
