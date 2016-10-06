9e60f4c9ee946f3b67caf7db9668db37b56d8428
jsekade <jsekade@gmail.com>
2016-10-06 20:52:49 +0200

    Mod Shopping_cart.rb

--- Shopping_cart.rb
+++ Shopping_cart.rb
@@ -2,18 +2,24 @@ class ShoppingCart
   def initialize
     @items = []
     @total_price = 0
+    @articles = 0
   end
   def add_item(item)
       @items.push(item)
+      @articles += 1
   end
   def checkout
-  total_price_cart = @total_price
-  @items.each do |item| 
-    total_price_cart += item.price
-  end
+    total_price_cart = @total_price
+    @items.each do |item| 
+      total_price_cart += item.price
+    end
+    if @articles > 5
+              discount = total_price_cart/10
+              total_price_cart -= discount
+          end
 
-  total_price_cart
-end
+    total_price_cart
+  end
 end
 
 class Item 
@@ -22,21 +28,21 @@ class Item
       @name = name
       @price = price
   end
-
-  def price
-      price = @price
-  end
 end
 
 class Houseware < Item
   def price
-      price = @price
+      if @price > 100
+        @price/5
+      else 
+        @price
+      end
   end
 end
 
 class Fruit < Item
   def price
-      price = @price
+      @price
   end
 end
 
@@ -48,6 +54,10 @@ vacuum_cleaner = Item.new("Vacuum Cleaner", 150)
 anchovies = Item.new("Anchovies", 2)
 joshs_cart.add_item(oj)
 joshs_cart.add_item(rice)
+joshs_cart.add_item(vacuum_cleaner)
+joshs_cart.add_item(anchovies)
+joshs_cart.add_item(banana)
+joshs_cart.add_item(banana)
 
 puts joshs_cart.checkout
 
