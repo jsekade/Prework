b028e3d9ab55bc25d6e2eefdd96b41151a7f7712
jsekade <jsekade@gmail.com>
2016-10-07 11:43:40 +0200

    Add Ruby

---
+++ Ruby/wordsorting.rb
@@ -0,0 +1,11 @@
+def wordsorting
+	puts "que tengas un buen dia"
+
+word = 'que, tengas, un, buen, dia'
+word_array = word.split(', ')
+wordsort = word_array.sort
+
+print wordsort
+end
+
+wordsorting
